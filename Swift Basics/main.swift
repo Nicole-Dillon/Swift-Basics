77bea9ddd6339f5a0dc5e45b814d4f12ebc178ca
Nicole Dillon <nkdvgd@mail.missouri.edu>
2019-02-08 05:18:03 -0600

    initial commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Nicole Dillon on 2/8/19.
+//  Copyright © 2019 Nicole Dillon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
